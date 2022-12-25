86147473408d23df30321df53c43c5212ca216fd
isabelpham <inpham@uci.edu>
2022-12-24 17:07:08 -0800

    Created Nav Bar

---
+++ script.js
@@ -0,0 +1,8 @@
+// hides and unhides the contents in the hamburger menu 
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+} )
