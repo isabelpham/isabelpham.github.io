456277800bb8c16d178526319a67f8752eabc58d
isabelpham <inpham@uci.edu>
2023-02-19 22:23:29 -0800

    Finished AFC case study.

--- script.js
+++
@@ -1,8 +0,0 @@
-// hides and unhides the contents in the hamburger menu 
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-} )
